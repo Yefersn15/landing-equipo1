6b66dd9b2b55941c5353ba1e9711c355b4941604
Yefersn15 <yefersonandres225@gmail.com>
2025-04-08 16:03:43 -0500

    Actualizacion 2 de los productos

--- ProductosDestacados/Lego_Ramo_de_Flores/JS/script.js
+++ ProductosDestacados/Lego_Ramo_de_Flores/JS/script.js
@@ -11,7 +11,7 @@ function toggleBuscador() {
 let lastScrollTop = 0;
 let barraAnuncios = document.querySelector(".barra-anuncios");
 let encabezado = document.querySelector(".encabezado");
-let producto = document.querySelector(".producto-top");
+let producto = document.querySelector(".productotop");
 
 window.addEventListener("scroll", function () {
   let currentScroll = window.scrollY;
@@ -23,26 +23,29 @@ window.addEventListener("scroll", function () {
   } else {
     barraAnuncios.style.top = "0px";
     encabezado.style.top = "27px";
-    producto.style.top = "90px";
+    producto.style.top = "100px";
   }
 
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 });
 
 var miniaturas = document.querySelectorAll('.miniatura');
-var contenedorPrincipal = document.querySelector('.imagen-principal');
+var contenedorPrincipal = document.querySelector('.imagen-principal'); // contenedor, no directamente <img>
 
 for (var i = 0; i < miniaturas.length; i++) {
   var miniatura = miniaturas[i];
 
   miniatura.onclick = function () {
+    // Quitar clase activa de todas
     for (var j = 0; j < miniaturas.length; j++) {
       miniaturas[j].classList.remove('activa');
     }
     this.classList.add('activa');
 
+    // Limpiar contenido anterior
     contenedorPrincipal.innerHTML = '';
 
+    // Obtener contenido de la miniatura
     var imagen = this.querySelector('img');
     var video = this.querySelector('video');
 
